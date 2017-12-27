8a502e5ba1bca601bec1c31ec3edc5000e4c0105
Georg Dangl <georg@dangl.me>
2017-12-27 15:56:26 +0100

    Add C# project with xUnit Theory Data attribute for NaughtyStrings

---
+++ NaughtyStringFactory.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace big_list_of_naughty_strings
+{
+    public class NaughtyStringFactory
+    {
+        public static IReadOnlyList<string> NaughtyStrings { get; }
+
+        static NaughtyStringFactory()
+        {
+            var naughtyStrings = GetNaughtyStringsFromEmbeddedResource();
+            NaughtyStrings = new ReadOnlyCollection<string>(naughtyStrings);
+        }
+
+        private static List<string> GetNaughtyStringsFromEmbeddedResource()
+        {
+            var jsonString = GetJsonStringFromEmbeddedResource();
+            var deserializedList = JsonConvert.DeserializeObject<List<string>>(jsonString);
+            return deserializedList;
+        }
+
+        private static string GetJsonStringFromEmbeddedResource()
+        {
+            using (var resourceStream = GetJsonStreamFromEmbeddedResource())
+            {
+                using (var streamReader = new StreamReader(resourceStream))
+                {
+                    var fileString = streamReader.ReadToEnd();
+                    return fileString;
+                }
+            }
+        }
+
+        private static Stream GetJsonStreamFromEmbeddedResource()
+        {
+            var assembly = typeof(NaughtyStringFactory).GetTypeInfo().Assembly;
+            var resourceName = $"{nameof(big_list_of_naughty_strings)}.blns.json"
+                .Replace("_", "-");
+            var resourceStream = assembly.GetManifestResourceStream(resourceName);
+            return resourceStream;
+        }
+    }
+}
---
+++ NaughtyStrings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace big_list_of_naughty_strings
+{
+    public sealed class NaughtyStrings : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            return NaughtyStringFactory
+                .NaughtyStrings
+                .Select(naughtyString => new object[] {naughtyString});
+        }
+    }
+}
