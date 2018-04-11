08033e42b9bc5d3a1e1d06a325631bc50976bd7a
Georg Dangl <georg@dangl.me>
2018-04-11 16:29:02 +0200

    Update xUnit data generation to also include txt soure

--- NaughtyStringFactory.cs
+++ NaughtyStringFactory.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using Newtonsoft.Json;
 
@@ -17,6 +18,17 @@ namespace big_list_of_naughty_strings
         }
 
         private static List<string> GetNaughtyStringsFromEmbeddedResource()
+        {
+            var stringsFromJson = GetNaughtyStringsFromJson();
+            var stringsFromTxt = GetNaughtyStringsFromTxt();
+            var allStrings = stringsFromJson
+                .Concat(stringsFromTxt)
+                .Distinct()
+                .ToList();
+            return allStrings;
+        }
+
+        private static List<string> GetNaughtyStringsFromJson()
         {
             var jsonString = GetJsonStringFromEmbeddedResource();
             var deserializedList = JsonConvert.DeserializeObject<List<string>>(jsonString);
@@ -42,6 +54,27 @@ namespace big_list_of_naughty_strings
                 .Replace("_", "-");
             var resourceStream = assembly.GetManifestResourceStream(resourceName);
             return resourceStream;
+        }
+
+        private static List<string> GetNaughtyStringsFromTxt()
+        {
+            var assembly = typeof(NaughtyStringFactory).GetTypeInfo().Assembly;
+            var resourceName = $"{nameof(big_list_of_naughty_strings)}.blns.txt"
+                .Replace("_", "-");
+            var strings = new List<string>();
+            using (var resourceStream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (var streamReader = new StreamReader(resourceStream))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        var currentString = streamReader.ReadLine();
+                        strings.Add(currentString);
+                    }
+
+                }
+            }
+            return strings;
         }
     }
 }
